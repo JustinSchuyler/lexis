eab9b0725dba461a64234d899c539d8c7c77f899
Justin Schuyler <justin.r.schuyler@gmail.com>
2019-06-23 13:01:12 -0600

    feat(context menu): launch landing page

--- background.js
+++ background.js
@@ -1,3 +1,16 @@
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.contextMenus.create({
+        id: 'add',
+        title: 'Add \'%s\' to Lexis',
+        type: 'normal',
+        contexts: [ 'selection' ]
+    });
+});
+
+chrome.contextMenus.onClicked.addListener((item) => {
+    chrome.tabs.create({ url: chrome.runtime.getURL('landing.html#' + item.selectionText) }); 
+})
+
 chrome.browserAction.onClicked.addListener(() => {
     chrome.tabs.create({ url: chrome.runtime.getURL('landing.html') });
 });

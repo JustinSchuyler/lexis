5daef37d3e27995b2a01e34a4fbc78fb185ee5ae
Justin Schuyler <justin.r.schuyler@gmail.com>
2019-07-02 20:03:06 -0600

    refactor(project): move react to root

--- landing.js
+++
@@ -1,7 +0,0 @@
-chrome.storage.sync.get(['selection'], ({ selection }) => {
-    console.log('result:', selection);
-    let title = document.querySelector('#title');
-    title.textContent = selection.word;
-    let context = document.querySelector('#context');
-    context.textContent = selection.context;
-});

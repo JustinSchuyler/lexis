c6aa78290b49dd632a79cace73946b0b5eae95dd
Justin Schuyler <justin.r.schuyler@gmail.com>
2019-07-06 15:26:53 -0600

    style(selection): emphasize context accordion

--- src/Selection.js
+++ src/Selection.js
@@ -20,11 +20,16 @@ class Selection extends React.Component {
 
     render() {
         const context = (this.state.open) ? this.props.selection.context : this.props.selection.sentence;
+        const liClasses = (this.state.open) ? 'selection open' : 'selection';
+        const title = (this.state.open) ? 'Collapse context' : 'Click for context';
 
         return (
-            <li className="selection" onClick={this.handleClick}>
+            <li className={liClasses} onClick={this.handleClick} title={title}>
                 <h2><a href={this.props.selection.url} target="_blank" rel="noopener noreferrer">{this.props.selection.word}</a></h2>
-                <div><i className="material-icons">keyboard_arrow_right</i>{context}</div>
+                <div>
+                    <i className="material-icons">keyboard_arrow_right</i>
+                    <span>{context}</span>
+                </div>
             </li>
         );
     }

8ad6e24cc78a47cc65a8cd4bcb85ca13b05985e8
Justin Schuyler <justin.r.schuyler@gmail.com>
2019-07-06 16:13:42 -0600

    feat(selection): add external link for google

--- src/Selection.js
+++ src/Selection.js
@@ -38,7 +38,10 @@ class Selection extends React.Component {
 
         return (
             <li className={liClasses} onClick={this.handleClick} title={title}>
-                <h2><a href={this.props.selection.url} target="_blank" rel="noopener noreferrer">{this.props.selection.word}</a></h2>
+                <h2>
+                    <a href={this.props.selection.url} target="_blank" rel="noopener noreferrer">{this.props.selection.word}</a>
+                    <a className="sub-link" href={'https://google.com/search?q=' + this.props.selection.word} target="_blank" rel="noopener noreferrer"><i className="material-icons">link</i> Google</a>
+                </h2>
                 <div>
                     <i className="material-icons">keyboard_arrow_right</i>
                     <span>{formattedContext}</span>

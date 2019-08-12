705dbb9ccc8b5f4aec0232cb35572f57c3be3fd0
Justin Schuyler <justin.r.schuyler@gmail.com>
2019-08-11 19:46:43 -0600

    refactor(selection): use hooks for state

--- src/Selection.js
+++ src/Selection.js
@@ -1,24 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Selection.css';
 
-class Selection extends React.Component {
-    constructor(props) {
-        super(props);
+function Selection(props) {
+    const [open, setOpen] = useState(false);
 
-        this.state = {
-            open: false
-        };
+    const handleClick = () => {
+        setOpen(!open);
+    };
 
-        this.handleClick = this.handleClick.bind(this);
-    }
-
-    handleClick() {
-        this.setState({
-            open: !this.state.open
-        });
-    }
-
-    emphasizeWord(word, context) {
+    const emphasizeWord = (word, context) => {
         const index = context.indexOf(word);
 
         return (
@@ -28,27 +18,25 @@ class Selection extends React.Component {
                 {context.substr(index + word.length)}
             </span>
         );
-    }
+    };
 
-    render() {
-        const context = (this.state.open) ? this.props.selection.context : this.props.selection.sentence;
-        const formattedContext = this.emphasizeWord(this.props.selection.word, context);
-        const liClasses = (this.state.open) ? 'selection open' : 'selection';
-        const title = (this.state.open) ? 'Collapse context' : 'Click for context';
+    const context = (open) ? props.selection.context : props.selection.sentence;
+    const formattedContext = emphasizeWord(props.selection.word, context);
+    const liClasses = (open) ? 'selection open' : 'selection';
+    const title = (open) ? 'Collapse context' : 'Click for context';
 
-        return (
-            <li className={liClasses} onClick={this.handleClick} title={title}>
-                <h2>
-                    <a href={this.props.selection.url} target="_blank" rel="noopener noreferrer">{this.props.selection.word}</a>
-                    <a className="sub-link" href={'https://google.com/search?q=' + this.props.selection.word} target="_blank" rel="noopener noreferrer"><i className="material-icons">link</i> Google</a>
-                </h2>
-                <div>
-                    <i className="material-icons">keyboard_arrow_right</i>
-                    <span>{formattedContext}</span>
-                </div>
-            </li>
-        );
-    }
+    return (
+        <li className={liClasses} onClick={handleClick} title={title}>
+            <h2>
+                <a href={props.selection.url} target="_blank" rel="noopener noreferrer">{props.selection.word}</a>
+                <a className="sub-link" href={'https://google.com/search?q=' + props.selection.word} target="_blank" rel="noopener noreferrer"><i className="material-icons">link</i> Google</a>
+            </h2>
+            <div>
+                <i className="material-icons">keyboard_arrow_right</i>
+                <span>{formattedContext}</span>
+            </div>
+        </li>
+    );
 }
 
 export default Selection;
